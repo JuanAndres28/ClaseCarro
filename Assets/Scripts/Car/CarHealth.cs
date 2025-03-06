f0111474c528c789aa6f44913731a90f500098e8
JuanAndres28 <jbmorales165@gmail.com>
2025-03-06 17:00:49 -0500

    Nivel1
    Jardin ya importado, con el pato y su movimiento mejorado

--- Assets/Scripts/Car/CarHealth.cs
+++ Assets/Scripts/Car/CarHealth.cs
@@ -5,17 +5,14 @@ using System.Collections.Generic;
 public class CarHealth : MonoBehaviour, IDamageable
 {
     [Header("Config")]
-    [SerializeField] private PlayerStats stats; 
+    [SerializeField] private PlayerStats stats;
     [SerializeField] private Transform spawnCarPosition;
-    //public GameObject miniToyPrefab; // Prefab del objeto que seguirá al jugador
-    //private GameObject currentMiniToy; // Referencia al objeto recogido
 
+    private Vector3 initialPosition;
 
-     private Vector3 initialPosition;
-
-     private void Update()
+    private void Update()
     {
-       if (stats.Health <= 0f)
+        if (stats.Health <= 0f)
         {
             PlayerDead();
         }
@@ -31,10 +28,9 @@ public class CarHealth : MonoBehaviour, IDamageable
         }
     }
 
-
-    public void TakeDamage(float amount)  
+    public void TakeDamage(float amount)
     {
-        if (stats.Health <= 0f) return; 
+        if (stats.Health <= 0f) return;
         stats.Health -= amount; // Reducir la salud
         Debug.Log("vida" + stats.Health);
         if (stats.Health <= 0f) // Verificar si el jugador sigue vivo
@@ -45,68 +41,34 @@ public class CarHealth : MonoBehaviour, IDamageable
     }
 
     private void PlayerDead()
-{
-    Debug.Log("el jugador murio");
-    //quitar movimiento
-    Rigidbody rb = GetComponent<Rigidbody>();
-    rb.linearVelocity = Vector3.zero;
-    rb.isKinematic = true;
-    //mostrar una img? audio?, etc.
-    //StartCoroutine(RespawnCoroutine());
-}
-
-public void AddHealth(float amount)
     {
+        Debug.Log("el jugador murio");
 
+        // Obtener el Rigidbody
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        // Detener la velocidad
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        // Mostrar una img, audio, etc.
+        // StartCoroutine(RespawnCoroutine());
+    }
+
+    public void AddHealth(float amount)
+    {
         stats.MiniToys += amount;
         stats.Health += amount;
         Debug.Log("vida" + stats.Health);
-        Debug.Log("MiniToys" + stats.MiniToys);  
-        /*if (stats.Health > stats.MaxHealth)
-        {
-            stats.Health = stats.MaxHealth;
-        } */
+        Debug.Log("MiniToys" + stats.MiniToys);
     }
 
-private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("MiniToys"))
         {
-            // Solo recoger si no hay un mini toy ya recogido
-            //if (currentMiniToy == null){
-                AddHealth(1);
-                Destroy(collision.gameObject); // Destruir el miniToy una vez recogido
-                //CollectMiniToy();}
+            AddHealth(1);
+            Destroy(collision.gameObject); // Destruir el miniToy una vez recogido
         }
     }
-
-
-/*private void CollectMiniToy()
-{
-    if (currentMiniToy == null)
-        {
-            // Instanciar el objeto recogido
-            currentMiniToy = Instantiate(miniToyPrefab, transform.position, Quaternion.identity);
-            currentMiniToy.GetComponent<MiniToys>().SetTarget(transform); // Pasar la referencia del jugador
-        }
-}*/ 
-/*private IEnumerator RespawnCoroutine()
-    {
-        // Opcional: Esperar antes de reaparecer
-        yield return new WaitForSeconds(2f);
-
-        // Reaparecer al jugador en el punto de reaparición
-        if (spawnCarPosition;!= null)
-        {
-            transform.position = spawnCarPosition.position;
-        }
-        else
-        {
-            transform.position = initialPosition; // Reaparecer en la posición inicial si no hay punto de reaparición definido
-        }
-
-        stats.Health = 1;
-
-    }*/
-
 }
