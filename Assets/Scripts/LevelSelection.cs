8992fa99d167110895832966618037cdac10ffc7
JuanAndres28 <jbmorales165@gmail.com>
2025-03-08 14:14:05 -0500

    Menús de Selección
    Ambos menús de selección ya funcionales, falta mejorar los sprites y el tema del algún botón del HUD.

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -14,20 +14,26 @@ public class LevelSelection : MonoBehaviour
     // Texto para mostrar el nombre del nivel seleccionado.
     public TextMeshProUGUI textoNivel;
 
+    // Imagen para mostrar el nivel seleccionado.
+    public Image imagenNivel;
+
+    // Lista de imágenes correspondientes a cada nivel.
+    public Sprite[] imagenesNiveles;
+
     // Nombre de la escena de selección de carros.
     public string nombreEscenaSeleccionCarro = "SeleccionCarro";
 
     private void Start()
     {
-        // Inicializa el texto con el primer nivel.
-        ActualizarTextoNivel();
+        // Inicializa el texto y la imagen con el primer nivel.
+        ActualizarNivel();
     }
 
     // Método para cambiar al siguiente nivel.
     public void SiguienteNivel()
     {
         nivelIndex = (nivelIndex + 1) % niveles.Length; // Cicla entre los niveles.
-        ActualizarTextoNivel();
+        ActualizarNivel();
     }
 
     // Método para seleccionar el nivel y cambiar a la escena de selección de carros.
@@ -41,9 +47,13 @@ public class LevelSelection : MonoBehaviour
         SceneManager.LoadScene(nombreEscenaSeleccionCarro);
     }
 
-    // Actualiza el texto del nivel seleccionado.
-    private void ActualizarTextoNivel()
+    // Actualiza el texto y la imagen del nivel seleccionado.
+    private void ActualizarNivel()
     {
         textoNivel.text = niveles[nivelIndex];
+        if (imagenesNiveles.Length > nivelIndex)
+        {
+            imagenNivel.sprite = imagenesNiveles[nivelIndex];
+        }
     }
 }
